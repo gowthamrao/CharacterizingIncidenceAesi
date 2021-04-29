d9c8c6c9b128c4e8bb93ff1ac0d1bf173f086618
Gowtham Rao <gowthamrao@gmail.com>
2021-04-29 12:59:09 +0530

    Made entire app reactive

--- ui.R
+++ ui.R
@@ -1,27 +1,10 @@
 ohdsiBlueHex <- "#20425A"
 ohdsiLightYellowHex <- "FBC209"
-
-addInfo <- function(item, infoId, class = NULL, style = NULL) {
-  if (is.null(class)) {
-    class = "badge pull-right action-button"
-  }
-  if (is.null(style)) {
-    style = "padding: 1px 6px 2px 6px; background-color: steelblue;"
-  }
-  infoTag <- tags$small(class = class,
-                        style = style,
-                        type = "button", 
-                        id = infoId,
-                        "i")
-  item$children[[1]]$children <- append(item$children[[1]]$children, list(infoTag))
-  return(item)
-}
-
 dashboardPage(
   dashboardHeader(
-    title = "EHDEN RA DUS",
+    title = "Incidence AESI",
     tags$li(div(img(src = 'logo.png',
-                    title = "EHDEN Rheumatoid Arthritis Drug Utilization Study", 
+                    title = "Characterizing Incidence AESI", 
                     height = "40px", 
                     width = "40px"),
                 style = "padding-top:0px; padding-bottom:0px;"),
@@ -30,25 +13,30 @@ dashboardPage(
   dashboardSidebar(
     tags$head(tags$style(HTML(paste0('.main-header { background-color: ', ohdsiBlueHex, '; }')))),
     sidebarMenu(id = "tabs",
-                menuItem("About", tabName = "about"),
-                menuItem("csDMARD Totals", tabName = "totals"),
-                menuItem("Secular Trends", tabName = "trends"),
-                menuItem("Database information", tabName = "databaseInformation")#,
+                menuItem("Results", tabName = "results"),
+                menuItem("About", tabName = "about")
     )
   ),
   dashboardBody(
-    tags$head(
-      tags$link(rel="stylesheet", type="text/css", href="ohdsi.css")
-    ),
-    ### changing theme
     tabItems(
       tabItem(tabName = "about",
               includeMarkdown("md/about.md")
       ),
-     tabItem(tabName = "databaseInformation",
-              downloadButton("dlDatabaseInformation", "Download Data"),
-              htmlOutput("borderDatabaseInformation"),
-              dataTableOutput("databaseInformationTable")
+     tabItem(tabName = "results",
+             column(4,
+             shinyWidgets::pickerInput(inputId = "sexFilter",label = "Filter by Sex",choices = c(),selected = c(),multiple = TRUE)
+             ),
+             column(4,
+             shinyWidgets::pickerInput(inputId = "ageFilter",label = "Filter by Age Group",choices = c(),selected = c(),multiple = TRUE)
+             ),
+             column(4,
+             shinyWidgets::pickerInput(inputId = "databaseFilter",label = "Filter by Database",choices = c(),selected = c(),multiple = TRUE)
+             ),
+             tabsetPanel(type = "tabs",
+                         tabPanel("Plot", plotOutput(outputId = "outputPlot",height = "1000px")),
+                         tabPanel("Table", DT::DTOutput("resultTable"))
+             )
+            
       )
     )
   )
